190d6832e3c726f4b9e978db61d239e8e06f4346
klaud <klaudiaforysiak@gmail.com>
2024-02-11 16:21:51 +0000

    deleted items from list but not refreshing list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,21 +12,39 @@ function App() {
   const [text, setText] = useState("To-Do-List");
   const [items, setItems] = useState([]);
   const [selectedKey, setSelectedKey] = useState(null);
+  // const [selectCard, setSelectedCard] = useState(null);
 
+ 
+
+    const handleButtonClick = (index) => {
+    setSelectedKey(index);
+  };
 
   function changeText(test) {
     setText(test);
   }
 
   function addItem(inputText) {
-    setItems(buttonWeek[selectedKey].values.push(inputText));
-    console.log(buttonWeek);
+    const arr = buttonWeek[selectedKey].values;
+    setItems(arr.push({ id: arr.length + 1 ,val:inputText}));
+    console.log(arr)
   }
 
-  const handleButtonClick = (index) => {
-    setSelectedKey(index);
-  };
 
+   
+  // function handleSelectedCard(i){
+  //   setSelectedCard(i)
+  // }
+
+
+  
+
+  // function deleteItem(deletedItem) {
+  //   const arr =  buttonWeek[selectedKey].values
+  //   const update = arr.filter(a => a.id !== deletedItem );
+  //   setItems(update);
+  //   console.log(update)
+  // }
   
 
   return (
@@ -52,18 +70,21 @@ function App() {
       </Row>
       <MainCard title={text} onAddItem={addItem} />
       <Row className=" container mt-5 justify-content-center " style={{margin : "auto"}}>
-      {buttonWeek.map((variant) => {
+      {buttonWeek.map((variant, index) => {
         return (
           <Cards
             item={items}
-            things={variant.values}
             titleOfCard={variant.dayOfWeek}
             bg={variant.type}
-            key={variant.id}
-            id={variant.id}
+            key={index}
+            id={index}
             text={variant.type === "light" ? "dark" : "white"}
             isBigger={selectedKey === variant.id}
             values = {variant.values}
+            // onDelete = {deleteItem}
+            // onSelected = {handleSelectedCard}
+          //  
+      
           
           />
         );
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,44 +1,64 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 
 function Cards(props) {
 
-  return (
-   
-    <>
-     <Col className="col-4 text-center d-flex justify-content-center">
-      <Card
-      
-        bg={props.bg}
-        // text={props.text}
-        className= {`mb-3 bg-${props.variant}.bg-gradiant  bg-opacity-75`}
-        style={{
-        width: props.isBigger ? '350px' : '300px',
-        height: props.isBigger ? '450px' : '400px',
-        transition: 'width 0.3s, height 0.3s', // Add smooth transition effect
-        color:"black",
-      
-      }}
-      >
-        <Card.Header className="list-text-style"> {props.titleOfCard} </Card.Header>{" "}
-        <Card.Body>
-        <Card.Text>
-      <ul>
-     {props.values.map((todoItem) =>{
-        return <li className="list-text-style" style = {{color: "white"}}>
-           { todoItem}
-        </li>
-      })} 
-      
-      </ul> 
+  const [list, updateList] = useState(props.values)
 
-      
-      </Card.Text>  
-      
-        </Card.Body>{" "}
-      </Card>
+function deletItem(value){
+
+  console.log(list, 'c')
+  let temp = list.filter(function(item) {
+    return item !== value
+})
+  updateList(temp
+     )
+
+}
+
+  return (
+    <>
+      <Col className="col-4 text-center d-flex justify-content-center">
+        <Card
+          bg={props.bg}
+          // text={props.text}
+          className={`mb-3 bg-${props.variant}.bg-gradiant  bg-opacity-75`}
+          style={{
+            width: props.isBigger ? "350px" : "300px",
+            height: props.isBigger ? "450px" : "400px",
+            transition: "width 0.3s, height 0.3s", // Add smooth transition effect
+            color: "black",
+          }}
+        >
+          <Card.Header className="list-text-style">
+            {" "}
+            {props.titleOfCard}{" "}
+          </Card.Header>{" "}
+          <Card.Body>
+            <Card.Text>
+              <ul>
+                {list.map((todoItem) => {
+                  return (
+                    <div
+              
+                      
+                    >
+                      <li
+                        className="list-text-style"
+                        style={{ color: "white" }}
+                        onClick={()=>deletItem(todoItem)}
+                      >
+                        {todoItem.val}{" "}
+                      </li>
+                    </div>
+                  );
+                })}
+              </ul>
+            </Card.Text>
+          </Card.Body>{" "}
+        </Card>
       </Col>
     </>
   );
